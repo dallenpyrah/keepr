514df0d50d214fa4bd7e2a32d3bf2ee22955271e
Dallen Pyrah <dallenpyrah@gmail.com>
2021-04-18 12:41:57 -0600

    Added try catches to all services.

--- client/src/services/KeepsService.js
+++ client/src/services/KeepsService.js
@@ -13,20 +13,32 @@ class KeepsService {
   }
 
   async deleteKeep(id) {
-    await api.delete('api/keeps/' + id)
-    this.getAllKeeps()
+    try {
+      await api.delete('api/keeps/' + id)
+      this.getAllKeeps()
+    } catch (error) {
+      logger.log(error)
+    }
   }
 
   async createKeep(newKeep) {
-    const res = await api.post('api/keeps', newKeep)
-    AppState.profileKeeps = [...AppState.profileKeeps, res.data]
+    try {
+      const res = await api.post('api/keeps', newKeep)
+      AppState.profileKeeps = [...AppState.profileKeeps, res.data]
+    } catch (error) {
+      logger.log(error)
+    }
   }
 
   async editKeepCount(id, editKeep) {
-    editKeep.keeps++
-    const res = await api.put('api/keeps/' + id, editKeep)
-    console.log(res.data)
-    AppState.profileKeeps = res.data
+    try {
+      editKeep.keeps++
+      const res = await api.put('api/keeps/' + id, editKeep)
+      console.log(res.data)
+      AppState.profileKeeps = res.data
+    } catch (error) {
+      logger.log(error)
+    }
   }
 }
 
--- client/src/services/ProfilesService.js
+++ client/src/services/ProfilesService.js
@@ -1,21 +1,34 @@
 import { AppState } from '../AppState'
+import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
 class ProfilesService {
   async GetUserProfile(id) {
-    const res = await api.get('api/profiles/' + id)
-    AppState.profile = res.data
+    try {
+      const res = await api.get('api/profiles/' + id)
+      AppState.profile = res.data
+    } catch (error) {
+      logger.log(error)
+    }
   }
 
   async getUserKeeps(id) {
-    const res = await api.get('api/profiles/' + id + '/keeps')
-    AppState.profileKeeps = res.data
+    try {
+      const res = await api.get('api/profiles/' + id + '/keeps')
+      AppState.profileKeeps = res.data
+    } catch (error) {
+      logger.log(error)
+    }
   }
 
   async getUserVaults(id) {
-    const res = await api.get(`api/profiles/${id}/vaults`)
-    AppState.profileVaults = res.data
-    AppState.isLoading = false
+    try {
+      const res = await api.get(`api/profiles/${id}/vaults`)
+      AppState.profileVaults = res.data
+      AppState.isLoading = false
+    } catch (error) {
+      logger.log(error)
+    }
   }
 }
 
